0b64f796c34d0e3b5dd4df3f22e4d85c56185c9b
ety001 <ety001@qq.com>
2012-12-17 17:31:50 +0800

    new

--- app.js
+++ app.js
@@ -4,44 +4,63 @@
  */
 
 var express = require('express')
-	, routes = require('./routes')
-	, user = require('./routes/user')
+	, note_index = require('./controller/index')
+	, user = require('./controller/user')
 	, http = require('http')
 	, path = require('path')
 	, MongoStore = require('connect-mongo')(express)
-	, setting = require('./setting');
-//console.log(MongoStore);
+	, settings = require('./settings')
+	, partials = require('express-partials')
+	, flash = require('connect-flash');
 var app = express();
 
 app.configure(function(){
 	//server port
-	app.set('port', process.env.PORT || setting.port);
+	app.set('port', process.env.PORT || settings.port);
 	//view dir
-	app.set('views', __dirname + setting.view_dir + setting.templates_dir);
+	app.set('views', __dirname + settings.view_dir + settings.templates_dir);
 	//view engine
 	app.set('view engine', 'ejs');
 	//set layout
 	app.set('view options',{layout:'layout'});
 	app.use(express.logger('dev'));
+	app.use(partials());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
+	app.use(flash());//flush must beford session
 	app.use(express.cookieParser());
 	app.use(express.session({
-		secret: setting.cookieSecret,
+		secret: settings.cookieSecret,
 		store: new MongoStore({
-			db: setting.db
+			db: settings.db
 		})
 	}));
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public')));
 });
-console.log(app.get('view options'));
+
+app.use(function(req, res, next){
+	app.locals.email = req.session.email;
+	//res.locals.uid = req.session.index;
+	app.locals.session = req.session;
+	app.locals.csrf = req.session ? req.session._csrf : '';
+	app.locals.email_error = req.flash('email_error').length ? req.flash('email_error') : '';
+	app.locals.pass_error = req.flash('pass_error').length ? req.flash('pass_error') : '';
+	console.log(app.locals.pass_error);
+	app.locals.error = req.flash('error').length ? req.flash('error') : '';
+	app.locals.success = req.flash('success').length ? req.flash('success') : '';
+	next();
+});
+
 app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', note_index.index);
 app.get('/users', user.list);
+app.post('/reg', user.add);
+app.get('/reg', user.reg);
+app.get('/signin', user.signin);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
